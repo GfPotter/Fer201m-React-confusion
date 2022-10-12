f3b0dd70f0228a74b77a3e9114e1aaa3d73791b3
Potter <quanpotterha@gmail.com>
2022-10-12 08:22:57 +0700

    Presentational and Container Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-
+import {
+    Card,
+    CardImg,
+    CardImgOverlay,
+    CardText,
+    CardBody,
+    CardTitle,
+  } from "reactstrap";
 
 
 class Menu extends Component {
