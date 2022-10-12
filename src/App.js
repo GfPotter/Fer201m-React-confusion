2501cdc24fb4f54f3d9e0a6fd895fad1ee51c14e
Potter <quanpotterha@gmail.com>
2022-10-12 08:04:23 +0700

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+
 import React, { Component } from "react";
 import Main from "./components/MainComponent";
-
+import "./App.css";
 class App extends Component {
   render() {
     return (
